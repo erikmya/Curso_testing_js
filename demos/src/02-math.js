7ddb366233abc738eb2d1f8144b7054ed9689584
erikmya <erik.maya@librerialeo.com.mx>
2022-12-06 17:49:48 -0600

    Clase 6 Primer test

---
+++ demos/src/02-math.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  return a + b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  if (b === 0) {
+    return null;
+  }
+  return a / b;
+}
+//RETO
+function rest(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  sum,
+  mult,
+  div,
+  rest
+};
