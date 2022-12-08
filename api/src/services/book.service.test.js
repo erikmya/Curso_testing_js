2d1272a0c2df56ea2689d0ffa104aefb9fb1b686
erikmya <erik.maya@librerialeo.com.mx>
2022-12-08 13:33:18 -0600

    Clase 18 🗣️ Mocking

--- api/src/services/book.service.test.js
+++ api/src/services/book.service.test.js
@@ -1,9 +1,24 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Altered Carbon',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
   describe('test for getBooks', () => {
     test('should return a list book', async () => {
@@ -12,7 +27,16 @@ describe('Test for BooksService', () => {
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test('should return a list book', async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Altered Carbon');
     });
   });
 });
